22e7d6b02372abb06cf0ea23c7fd704bc4ca10ca
henrisusanto <henri@member.id>
2022-10-08 06:03:30 +0700

    follow redirect to detect https

--- shopify-detector.php
+++ shopify-detector.php
@@ -80,6 +80,7 @@ add_action('rest_api_init', function () {
             ];
 
             $ch = curl_init();
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
             curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0');
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
